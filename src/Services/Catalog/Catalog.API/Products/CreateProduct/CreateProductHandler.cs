19e7b097ff2008cbb73ea247b74efad965711219
Mohamed Kassem <mohamed.k.kassem@pwc.com>
2025-03-23 14:43:40 +0200

    feat: Add inital marten setup

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -4,7 +4,7 @@ public record CreateProductCommand(string Name, List<string> Category, string De
     : ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler()
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -21,7 +21,13 @@ internal class CreateProductCommandHandler()
             ImageFile = command.ImageFile,
             Price = command.Price
         };
-        
+
+        // save to database
+        // No need to repo pattern,
+        // IDocumentSession is a unit of work pattern
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
         //return result
         return new CreateProductResult(product.Id);
     }
