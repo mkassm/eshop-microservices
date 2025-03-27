eea09febcdd7fd3b65ed0ee70f1e4f32c6a58bc0
Mohamed Kassem <mohamed.k.kassem@pwc.com>
2025-03-28 01:37:49 +0200

    feat: add UpdateProduct api

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -3,4 +3,6 @@ global using Mapster;
 global using MediatR;
 global using BuildingBlocks.CQRS;
 global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
 global using Marten;
+global using Marten.Pagination;
