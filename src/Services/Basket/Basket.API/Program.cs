5d43d0538c8a5da8bcd6240b2d9461dc412f6609
Mohamed Kassem <mohamed.k.kassem@pwc.com>
2025-04-06 19:20:33 +0200

    feat: add GetBasket + repository + redis cache

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,39 @@
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    // Default is AutoCreate.CreateOrUpdate
+    // It will create or update automatically when first api call
+    //opts.AutoCreateSchemaObjects = AutoCreate.All;
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "Basket";
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
