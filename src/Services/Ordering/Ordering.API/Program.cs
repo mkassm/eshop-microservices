5d79cb30677bfbc6d7f15502c8f12db6a24e155f
Mohamed Kassem <mohamed.k.kassem@pwc.com>
2025-04-26 16:01:54 +0300

    uncomment add api services

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services
     .AddApplicationServices(builder.Configuration)
     .AddInfrastructureServices(builder.Configuration)
-    /*.AddApiServices(builder.Configuration)*/;
+    .AddApiServices(builder.Configuration);
 
 // Add Services
 var app = builder.Build();
